e85e87adaf5d801652b03cd1fc09b5c85fc1ea53
skytomo <tomo1kawa.fullerene@gmail.com>
2021-05-08 17:08:51 +0900

    Change to gismuRelationsList

--- GismuRelatedWordsGenerator/Program.cs
+++ GismuRelatedWordsGenerator/Program.cs
@@ -16,144 +16,268 @@ namespace GismuRelatedWordsGenerator
 
     class Program
     {
-        private static readonly List<GismuRelation> gismuRelations = new List<GismuRelation> {
-            new GismuRelation {
-                Title = "色に関する同一構造のギスム",
-                Entries = new List<string> {
-                    "blabi",
-                    "blanu",
-                    "bunre",
-                    "cicna",
-                    "crino",
-                    "grusi",
-                    "narju",
-                    "nukni",
-                    "pelxu",
-                    "xekri",
-                    "xunre",
-                    "zirpu",
-                }
+        private static readonly List<List<GismuRelation>> gismuRelationsList = new List<List<GismuRelation>> {
+            new List<GismuRelation> {
+                new GismuRelation {
+                    Title = "動作に関するギスム",
+                    Entries = new List<string> {
+                        "bajra",
+                        "banro",
+                        "basti",
+                        "batci",
+                        "benji",
+                        "bevri",
+                        "bikla",
+                        "binxo",
+                        "canci",
+                        "canja",
+                        "carna",
+                        "carvi",
+                        "casnu",
+                        "catke",
+                        "catke",
+                        "catra",
+                        "cecla",
+                        "cikna",
+                        "cikre",
+                        "cilre",
+                        "cinba",
+                        "cinmo",
+                        "cirko",
+                        "ciska",
+                        "cisma",
+                        "citka",
+                        "ckasu",
+                        "cladu",
+                        "cliva",
+                        "cmila",
+                        "cmoni",
+                        "cpacu",
+                        "danfu",
+                        "dansu",
+                        "darxi",
+                        "dirce",
+                        "dunda",
+                        "falnu",
+                        "farlu",
+                        "fliba",
+                        "janli",
+                        "jersi",
+                        "jgari",
+                        "jinru",
+                        "jivbu",
+                        "klama",
+                        "lacpu",
+                        "lebna",
+                        "limna",
+                        "parji",
+                        "pencu",
+                        "ralte",
+                        "renro",
+                        "rirni",
+                        "sakli",
+                        "sanga",
+                        "sanji",
+                        "sanli",
+                        "satre",
+                        "senci",
+                        "stapa",
+                        "tadni",
+                        "tikpa",
+                        "toknu",
+                        "tunlo",
+                        "vamtu",
+                        "vasru",
+                        "vasxu",
+                        "vofli",
+                    }
+                },
             },
-            new GismuRelation {
-                Title = "衣類に関する同一構造のギスム",
-                Entries = new List<string> {
-                    "mapku",
-                    "creka",
-                    "kosta",
-                    "palku",
-                    "skaci",
-                    "cutci",
-                    "smoka",
-                    "dasri",
-                    "genxu",
-                    "grana",
-                    "jesni",
-                }
+            new List<GismuRelation> {
+                new GismuRelation {
+                    Title = "色に関する同一構造のギスム",
+                    Entries = new List<string> {
+                        "blabi",
+                        "blanu",
+                        "bunre",
+                        "cicna",
+                        "crino",
+                        "grusi",
+                        "narju",
+                        "nukni",
+                        "pelxu",
+                        "xekri",
+                        "xunre",
+                        "zirpu",
+                    }
+                },
+                new GismuRelation {
+                    Title = "物質の性質に関するギスム",
+                    Entries = new List<string> {
+                        "jdari",
+                        "pruni",
+                        "ranti",
+                        "viknu",
+                    }
+                },
             },
-            new GismuRelation {
-                Title = "国と地域に関する同一構造のギスム",
-                Entries = new List<string> {
-                    "baxso",
-                    "bemro",
-                    "bengo",
-                    "bindo",
-                    "brazo",
-                    "brito",
-                    "dotco",
-                    "dzipo",
-                    "filso",
-                    "fraso",
-                    "friko",
-                    "gento",
-                    "glico",
-                    "jerxo",
-                    "jordo",
-                    "jungo",
-                    "kadno",
-                    "ketco",
-                    "kisto",
-                    "latmo",
-                    "libjo",
-                    "lojbo",
-                    "lubno",
-                    "meljo",
-                    "merko",
-                    "mexno",
-                    "misro",
-                    "morko",
-                    "polno",
-                    "ponjo",
-                    "porto",
-                    "rakso",
-                    "ropno",
-                    "rusko",
-                    "sadjo",
-                    "semto",
-                    "sirxo",
-                    "skoto",
-                    "slovo",
-                    "softo",
-                    "spano",
-                    "sralo",
-                    "srito",
-                    "vukro",
-                    "xazdo",
-                    "xebro",
-                    "xelso",
-                    "xindo",
-                    "xispo",
-                    "xrabo",
-                    "xurdo",
-                }
+            new List<GismuRelation> {
+                new GismuRelation {
+                    Title = "数学に関するギスム",
+                    Entries = new List<string> {
+                        "namcu",
+                        "tanjo",
+                        "mekso",
+                        "sinso",
+                        "pilji",
+                        "radno",
+                        "frinu",
+                        "dugri",
+                        "saclu",
+                        "sumji",
+                        "xadba",
+                        "sumti",
+                        "tenfa",
+                        "klani",
+                        "kancu",
+                        "kanji",
+                        "fancu",
+                        "dilcu",
+                        "vimcu",
+                        "girzu",
+                    }
+                },
             },
-            new GismuRelation {
-                Title = "生体に関する同一構造のギスム",
-                Entries = new List<string> {
-                    "ciblu",
-                    "kalci",
-                    "pimlu",
-                    "pinca",
-                    "rectu",
-                    "sovda",
-                }
+            new List<GismuRelation> {
+                new GismuRelation {
+                    Title = "衣類に関する同一構造のギスム",
+                    Entries = new List<string> {
+                        "mapku",
+                        "creka",
+                        "kosta",
+                        "palku",
+                        "skaci",
+                        "cutci",
+                        "smoka",
+                        "dasri",
+                        "genxu",
+                        "grana",
+                        "jesni",
+                    }
+                },
             },
-            new GismuRelation {
-                Title = "味覚に関する同一構造のギスム",
-                Entries = new List<string> {
-                    "cpina",
-                    "titla",
-                    "kurki",
-                    "slari",
-                }
+            new List<GismuRelation> {
+                new GismuRelation {
+                    Title = "国と地域に関する同一構造のギスム",
+                    Entries = new List<string> {
+                        "baxso",
+                        "bemro",
+                        "bengo",
+                        "bindo",
+                        "brazo",
+                        "brito",
+                        "dotco",
+                        "dzipo",
+                        "filso",
+                        "fraso",
+                        "friko",
+                        "gento",
+                        "glico",
+                        "jerxo",
+                        "jordo",
+                        "jungo",
+                        "kadno",
+                        "ketco",
+                        "kisto",
+                        "latmo",
+                        "libjo",
+                        "lojbo",
+                        "lubno",
+                        "meljo",
+                        "merko",
+                        "mexno",
+                        "misro",
+                        "morko",
+                        "polno",
+                        "ponjo",
+                        "porto",
+                        "rakso",
+                        "ropno",
+                        "rusko",
+                        "sadjo",
+                        "semto",
+                        "sirxo",
+                        "skoto",
+                        "slovo",
+                        "softo",
+                        "spano",
+                        "sralo",
+                        "srito",
+                        "vukro",
+                        "xazdo",
+                        "xebro",
+                        "xelso",
+                        "xindo",
+                        "xispo",
+                        "xrabo",
+                        "xurdo",
+                    }
+                },
             },
-            new GismuRelation {
-                Title = "季節に関する同一構造のギスム",
-                Entries = new List<string> {
-                    "vensa",
-                    "crisa",
-                    "critu",
-                    "dunra",
-                }
+            new List<GismuRelation> {
+                new GismuRelation {
+                    Title = "生体に関する同一構造のギスム",
+                    Entries = new List<string> {
+                        "ciblu",
+                        "kalci",
+                        "pimlu",
+                        "pinca",
+                        "rectu",
+                        "sovda",
+                    }
+                },
             },
-            new GismuRelation {
-                Title = "時間帯に関する同一構造のギスム",
-                Entries = new List<string> {
-                    "murse",
-                    "cerni",
-                    "donri",
-                    "vanci",
-                    "nicte",
-                }
+            new List<GismuRelation> {
+                new GismuRelation {
+                    Title = "味覚に関する同一構造のギスム",
+                    Entries = new List<string> {
+                        "cpina",
+                        "titla",
+                        "kurki",
+                        "slari",
+                    }
+                },
             },
-            new GismuRelation {
-                Title = "期間に関する同一構造のギスム",
-                Entries = new List<string> {
-                    "snidu",
-                    "mentu",
-                    "cacra",
-                    "djedi",
-                    "masti",
-                }
+            new List<GismuRelation> {
+                new GismuRelation {
+                    Title = "季節に関する同一構造のギスム",
+                    Entries = new List<string> {
+                        "vensa",
+                        "crisa",
+                        "critu",
+                        "dunra",
+                    }
+                },
+                new GismuRelation {
+                    Title = "時間帯に関する同一構造のギスム",
+                    Entries = new List<string> {
+                        "murse",
+                        "cerni",
+                        "donri",
+                        "vanci",
+                        "nicte",
+                    }
+                },
+                new GismuRelation {
+                    Title = "期間に関する同一構造のギスム",
+                    Entries = new List<string> {
+                        "snidu",
+                        "mentu",
+                        "cacra",
+                        "djedi",
+                        "masti",
+                    }
+                },
             },
         };
 
@@ -162,20 +286,26 @@ namespace GismuRelatedWordsGenerator
             var json = File.ReadAllText(@"lojbantan-zei-jbovlaste.json");
             var dictionary = OneToManyJsonSerializer.Deserialize(json);
 
-            foreach (var gismuRelation in gismuRelations)
+            foreach (var gismuRelations in gismuRelationsList)
             {
-                var entries = gismuRelation.Entries;
-                var relations = entries.Select(
-                    gismu => new Relation
-                    {
-                        Title = gismuRelation.Title,
-                        Entry = dictionary.Words.First(word => word.Entry.Form == gismu).Entry
-                    }
-                );
-                foreach (var gismu in entries)
+                var relations = new List<Relation>();
+                foreach (var gismuRelation in gismuRelations)
                 {
-                    var word = dictionary.Words.First(word => word.Entry.Form == gismu);
-                    word.Relations = word.Relations.Union(relations).ToList();
+                    gismuRelation.Entries.ForEach(
+                        gismu => relations.Add(new Relation
+                        {
+                            Title = gismuRelation.Title,
+                            Entry = dictionary.Words.First(word => word.Entry.Form == gismu).Entry
+                        }
+                    ));
+                }
+                foreach (var gismuRelation in gismuRelations)
+                {
+                    foreach (var gismu in gismuRelation.Entries)
+                    {
+                        var word = dictionary.Words.First(word => word.Entry.Form == gismu);
+                        word.Relations = word.Relations.Union(relations).ToList();
+                    }
                 }
             }
 
